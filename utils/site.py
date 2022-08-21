749d0d1b0e26d16f6e1e730efa729a4c2d15f33a
Xiaozhe Yao <askxzyao@gmail.com>
2022-08-22 01:07:34 +0200

    add data table

--- utils/site.py
+++ utils/site.py
@@ -1,14 +1,10 @@
-# Doc
-
-### Update Status
-
-``` python
 import requests
+
 res = requests.post('https://planetd.shift.ml/site_stats', json={
   "site_identifier": "stanford.edu", # ethz.ch; osg-htc.org
-  "total_perfs": 0,
+  "total_perfs": 1000,
   "num_gpu": 0,
   "num_cpu": 0,
   "note": "string"
 })
-```
+
