c5d5250c3e061587760f6217bdaa0716bef15c0f
Xiaozhe Yao <askxzyao@gmail.com>
2022-08-22 01:23:47 +0200

    fix links

--- frontend/src/components/PMFooter.vue
+++ frontend/src/components/PMFooter.vue
@@ -1,6 +1,6 @@
 <template>
     <footer class="p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
-    <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="https://flowbite.com/" class="hover:underline">TOMA: Together Open Inference Program</a>. Made by DS3Lab, ETH Zurich.
+    <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="/" class="hover:underline">TOMA: Together Open Inference Program</a>. Made by <a href="https://ds3lab.com" class="hover:underline">DS3Lab, ETH Zurich</a>.
     </span>
     <ul class="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
         <li>
