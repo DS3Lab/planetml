f01c892f1b2ffa1dab9573d6ff3c66ee28a5be99
Xiaozhe Yao <askxzyao@gmail.com>
2022-08-22 01:18:16 +0200

    update resource view

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -11,7 +11,23 @@ function get_jobs_list() {
     return axios.get(tomaapi_endpoint + 'jobs')
 }
 
+function get_status_history() {
+    return axios.get(tomaapi_endpoint + 'site_stats')
+}
+
+function domain_to_name(domain) {
+    if (domain === 'ethz.ch') {
+        return 'ETH Zurich'
+    } else if (domain === 'stanford.edu') {
+        return 'Stanford University'
+    } else if (domain === 'osg-htc.org') {
+        return 'Open Science Grid'
+    }
+}
+
 export {
     get_site_status,
-    get_jobs_list
+    get_jobs_list,
+    get_status_history,
+    domain_to_name
 }
