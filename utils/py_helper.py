a4ffc643e86f2c2354867687ceceec224f0819c5
Xiaozhe Yao <askxzyao@gmail.com>
2022-08-22 22:09:38 +0200

    update schemas

--- utils/py_helper.py
+++ utils/py_helper.py
@@ -1,10 +1,12 @@
 import requests
 
 res = requests.post('https://planetd.shift.ml/site_stats', json={
-  "site_identifier": "stanford.edu", # ethz.ch; osg-htc.org
-  "total_perfs": 1000,
-  "num_gpu": 0,
-  "num_cpu": 0,
+  "site_identifier": 'ethz.ch',
+  "scheduler_type": 'slurm',
+  "total_tflops": 1000.0,
+  "avail_tflops": 1000.0,
+  "total_gpus": 20,
+  "avail_gpus": 20,
+  "resources": 20,
   "note": "string"
 })
-
