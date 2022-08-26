186bceba184412b00b1b863c25dcbb9bfdc8f3ba
Xiaozhe Yao <askxzyao@gmail.com>
2022-08-26 21:40:46 +0200

    limit query size to one day

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const tomaapi_endpoint = 'https://planetd.shift.ml/'
-// const tomaapi_endpoint = 'http://localhost:5000/'
+// const tomaapi_endpoint = 'https://planetd.shift.ml/'
+const tomaapi_endpoint = 'http://localhost:5000/'
 
 function get_site_status() {
     return axios.get(tomaapi_endpoint + 'sites')
