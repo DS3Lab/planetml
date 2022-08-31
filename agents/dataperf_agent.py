c5f0ac2ade03fe6389e20c646f9115542323859e
Xiaozhe Yao <askxzyao@gmail.com>
2022-08-31 22:40:52 +0200

    wraping up the dataperf agents

--- agents/dataperf_agent.py
+++ agents/dataperf_agent.py
@@ -7,9 +7,9 @@ import typer
 import requests
 from pydantic import BaseSettings
 from clients.LSFClient import LSFClient
-from rest.rest import update_job_status
 import time
-from loguru import logger
+from utils.rest import update_job_status
+from utils.pprint import print_table
 planetml_url = 'https://planetd.shift.ml/jobs'
 
 agent = typer.Typer()
@@ -34,6 +34,7 @@ def dispatch_job(lsf_client, job):
         f"mkdir {job['payload']['submission_id']} && cd {job['payload']['submission_id']} && wget {job['payload']['url']}?t=1234 -O {job['payload']['submission_id']}.zip && unzip {job['payload']['submission_id']}.zip && rm {job['payload']['submission_id']}.zip"
     )
     image_path = "/nfs/iiscratch-zhang.inf.ethz.ch/export/zhang/export/xiayao/projects/dataperf/images/"
+    
     results = lsf_client.execute(f"cd {job['payload']['submission_id']} && bsub -R singularity -n 32 /cluster/home/xiayao/.local/bin/mls.py run-pipeline --image-path {image_path}")
     
     job_id = results.split("<")[1].split(">")[0]
@@ -48,6 +49,17 @@ def check_job_status(lsf_client):
     records = json.loads(results)
     return records['RECORDS']
 
+def harvest_finished_run(lsf_client, job_id, submission_id, euler_job_id):
+    typer.echo(f"Checking is job is done successfully")
+    # now check if the job is done successfully
+    is_successful = lsf_client.is_successful(submission_id, euler_job_id)
+    if is_successful:
+        lsf_client.execute(f"cd {lsf_client.wd}/{submission_id} && mls.py upload-results --bucket dataperf-results --filename {submission_id}.zip")
+        typer.echo(f"Updating job {job_id} to finished")
+        update_job_status(job_id, processed_by=f"{euler_job_id}:euler.ethz.ch", status='finished')
+    else:
+        typer.echo(f"Updating job {job_id} to failed")
+        update_job_status(job_id, processed_by=f"{euler_job_id}:euler.ethz.ch", status='failed')
 
 @agent.command()
 def fetch():
@@ -74,6 +86,7 @@ def watch():
     typer.echo("Watching for new jobs")
     watch_job_map = {}
     while True:
+        # Dispatching new jobs
         response = requests.get(planetml_url).json()
         submitted_jobs = [x for x in response if x['status'] == 'submitted']
         settings = Settings()
@@ -89,11 +102,16 @@ def watch():
             processed_by = dispatch_job(lsf_client, each)
             watch_job_map[each['id']] = {
                 'processed_by': processed_by,
-                'status': 'queued'
+                'status': 'queued',
+                'submission_id': each['payload']['submission_id']
             }
+        # check jobs status
         status_records = check_job_status(lsf_client)
+        if len(status_records) > 0:
+            print_table(status_records, title="Jobs Status")
+            
         for each_record in status_records:
-            processed_by = f"{each['JOBID']}:{each['QUEUE']}:euler.ethz.ch"
+            processed_by = f"{each_record['JOBID']}:{each_record['QUEUE']}:euler.ethz.ch"
             still_running = False
             job_id = None
             for each in watch_job_map:
@@ -101,18 +119,23 @@ def watch():
                 if watch_job_map[each]['processed_by'] == processed_by:
                     still_running = True
                     job_id = each
-                    if each['status'] == "RUN":
+                    if each_record['STAT'] == "RUN":
                         new_status='running'
-                    elif each['status'] == "PEND":
+                    elif each_record['STAT'] == "PEND":
                         new_status='queued'
 
-            if still_running and new_status != 'queued':
-                typer.echo(f"Updating job {job_id} to {new_status}")
-                update_job_status(job_id, processed_by=processed_by, status=new_status)
-            else:
-                typer.echo(f"Updating job {job_id} to completed")
-                res = update_job_status(job_id, processed_by=processed_by, status='finished')
-                print(res.text)
+            if job_id is not None:
+                if still_running and new_status != 'queued':
+                    typer.echo(f"Updating job {job_id} to {new_status}")
+                    update_job_status(job_id, processed_by=processed_by, status=new_status)
+        # harvest finished runs
+        for each in watch_job_map:
+            processed_by = watch_job_map[each]['processed_by']
+            euler_jobid = processed_by.split(":")[0]
+            running_jobs = [x['JOBID'] for x in status_records]
+            if euler_jobid not in running_jobs:
+                typer.echo(f"Harvesting job {each}")
+                harvest_finished_run(lsf_client,each, watch_job_map[each]['submission_id'], euler_jobid)
         typer.echo(f"Sleeping for 10 seconds")
         time.sleep(10)
 
