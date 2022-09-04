e9c4c1107a956fa339e69bf25f5275bafbe96cbc
Xiaozhe Yao <askxzyao@gmail.com>
2022-09-05 00:18:21 +0200

    fetch jobs per 10 seconds

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -6,9 +6,9 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  title: 'TOMA',
+  tagline: 'Together Open Inferenece Program',
+  url: 'https://tomadocs.pages.dev',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -57,7 +57,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'My Site',
+        title: 'TOMA',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
