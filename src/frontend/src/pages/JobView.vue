f196004279ded49a96250ca1b330f494697ed99f
Xiaozhe Yao <askxzyao@gmail.com>
2022-09-05 00:40:56 +0200

    fix bugs

--- src/frontend/src/pages/JobView.vue
+++ src/frontend/src/pages/JobView.vue
@@ -55,10 +55,6 @@
                     </svg>
                     {{ status }}
                 </span>
-                <span v-else
-                    class="bg-blue-100 text-blue-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800">
-                    {{ status }}
-                </span>
             </template>
         </Vue3EasyDataTable>
     </div>
