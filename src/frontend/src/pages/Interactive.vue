fdf1afd3e8b57788011018bf272103a4db467bc3
Xiaozhe Yao <askxzyao@gmail.com>
2022-09-06 00:01:19 +0200

    update

--- src/frontend/src/pages/Interactive.vue
+++ src/frontend/src/pages/Interactive.vue
@@ -1,50 +1,81 @@
 <script setup>
-import { reactive } from "vue";
-import { useMainStore } from "@/stores/main";
-import {
-    mdiAccount,
-    mdiMail,
-} from "@mdi/js";
+import { ref } from "vue";
 import SectionMain from "@/components/SectionMain.vue";
 import CardBox from "@/components/CardBox.vue";
 import FormField from "@/components/FormField.vue";
-import FormControl from "@/components/FormControl.vue";
 import BaseButton from "@/components/BaseButton.vue";
 import BaseButtons from "@/components/BaseButtons.vue";
-import CodeEditor from 'simple-code-editor';
-
-const mainStore = useMainStore();
-const requestForm = reactive({
-    request_json: mainStore.request_json,
-});
-
-requestForm.request_json = {}
+import { PrismEditor } from 'vue-prism-editor';
+import 'vue-prism-editor/dist/prismeditor.min.css';
+import { highlight, languages } from 'prismjs/components/prism-core';
+import 'prismjs/components/prism-json';
+import 'prismjs/themes/prism-tomorrow.css';
+import { add_new_job } from '@/services/api';
+const request_json = ref(`
+{
+    "input": "The answer to the universe, and everything",
+    "model": "stable_diffusion",
+    "num_returns": 10
+}
+`)
 
 const submitPass = () => {
-    //
+    let request_payload = JSON.parse(request_json.value)
+    console.log(request_payload)
+    add_new_job(request_payload).then((response) => {
+        console.log(response)
+    })
 };
+
+
+function highlighter(code) {
+    return highlight(code, languages.json);
+}
+
 </script>
-    
+
     <template>
     <SectionMain>
         <div class="grid grid-cols-1 lg:grid-cols-2 gap-6">
             <CardBox>
                 <FormField label="Prompt" help="Required. Your Prompt">
-
-                    <CodeEditor></CodeEditor>
-
+                    <div class="content">
+                        <prism-editor class="my-editor" v-model="request_json"
+                            :highlight="highlighter" line-numbers>
+                        </prism-editor>
+                    </div>
                 </FormField>
-
                 <template #footer>
                     <BaseButtons>
-                        <BaseButton color="info" type="submit" label="Submit" />
+                        <BaseButton @click="submitPass" color="info"
+                            type="submit" label="Submit" />
                     </BaseButtons>
                 </template>
             </CardBox>
-
-            <CardBox is-form @submit.prevent="submitPass">
+            <CardBox>
                 Progress
             </CardBox>
         </div>
     </SectionMain>
 </template>
+
+
+<style>
+/* required class */
+.my-editor {
+    /* we dont use `language-` classes anymore so thats why we need to add background and text color manually */
+    background: #2d2d2d;
+    color: #ccc;
+
+    /* you must provide font-family font-size line-height. Example: */
+    font-family: Fira code, Fira Mono, Consolas, Menlo, Courier, monospace;
+    font-size: 14px;
+    line-height: 1.5;
+    padding: 5px;
+}
+
+/* optional class for removing the outline */
+.prism-editor__textarea:focus {
+    outline: none;
+}
+</style>
