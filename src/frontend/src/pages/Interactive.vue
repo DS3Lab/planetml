14f5deeaf13fbe679cfb75317491fe8f6414230e
Xiaozhe Yao <askxzyao@gmail.com>
2022-09-06 01:10:54 +0200

    update interface

--- src/frontend/src/pages/Interactive.vue
+++ src/frontend/src/pages/Interactive.vue
@@ -10,7 +10,8 @@ import 'vue-prism-editor/dist/prismeditor.min.css';
 import { highlight, languages } from 'prismjs/components/prism-core';
 import 'prismjs/components/prism-json';
 import 'prismjs/themes/prism-tomorrow.css';
-import { add_new_job } from '@/services/api';
+import { add_new_job, get_job_status } from '@/services/api';
+
 const request_json = ref(`
 {
     "input": "The answer to the universe, and everything",
@@ -19,14 +20,32 @@ const request_json = ref(`
 }
 `)
 
+const job_status = ref({
+    "id": "",
+    "status": "",
+    "processed_by": "",
+    "returned_payload": {"output":[[]]}
+})
+
+function update_job_status(job_id) {
+    get_job_status(job_id).then((response) => {
+        job_status.value.status = response.data.status
+        job_status.value.processed_by = response.data.processed_by
+        job_status.value.returned_payload = response.data.returned_payload
+    })
+}
+
 const submitPass = () => {
     let request_payload = JSON.parse(request_json.value)
-    
+
     add_new_job(request_payload).then((response) => {
-        console.log(response)
+        job_status.value.id = response.data.id
+        job_status.value.status = response.data.status
+        setInterval(() => {
+            update_job_status(job_status.value.id)
+        }, 5000)
     })
 };
-
 
 function highlighter(code) {
     return highlight(code, languages.json);
@@ -53,9 +72,59 @@ function highlighter(code) {
                 </template>
             </CardBox>
             <CardBox>
-                Progress
+                <div>
+                    <p v-if="job_status.id!==''"> >>> Your job is submitted, with the id {{ job_status.id
+                    }}</p>
+                    <p v-if="job_status.processed_by !=='' "> >>> Your job is running, on the queue {{ job_status.processed_by }}</p>
+                    <p v-if="job_status.status==='finished'"> >>> Your job is finished </p>
+                    <p v-if="job_status.status==='finished'"> >>> Your job returned {{ job_status.returned_payload }}
+                    </p>
+                </div>
+
             </CardBox>
         </div>
+        <div v-if="job_status.status==='finished'" id="animation-carousel" class="relative" data-carousel="static">
+            <!-- Carousel wrapper -->
+            <div class="relative h-56 overflow-hidden rounded-lg md:h-96" v-for="img_src in job_status.returned_payload.output[0]">
+                <div class="hidden duration-200 ease-linear" data-carousel-item>
+                    <img :src="img_src"
+                        class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                        alt="...">
+                </div>
+            </div>
+            <!-- Slider controls -->
+            <button type="button"
+                class="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+                data-carousel-prev>
+                <span
+                    class="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                    <svg aria-hidden="true"
+                        class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800"
+                        fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            stroke-width="2" d="M15 19l-7-7 7-7"></path>
+                    </svg>
+                    <span class="sr-only">Previous</span>
+                </span>
+            </button>
+            <button type="button"
+                class="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+                data-carousel-next>
+                <span
+                    class="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                    <svg aria-hidden="true"
+                        class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800"
+                        fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            stroke-width="2" d="M9 5l7 7-7 7"></path>
+                    </svg>
+                    <span class="sr-only">Next</span>
+                </span>
+            </button>
+        </div>
+
     </SectionMain>
 </template>
 
