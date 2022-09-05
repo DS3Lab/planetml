0da35e09c5772862dcadd1cc4dbe8307166ad3b4
Xiaozhe Yao <askxzyao@gmail.com>
2022-09-06 01:52:38 +0200

    fix a minor bug

--- src/frontend/src/pages/Interactive.vue
+++ src/frontend/src/pages/Interactive.vue
@@ -49,10 +49,6 @@ function highlighter(code) {
     return highlight(code, languages.json);
 }
 
-onMounted(() => {
-    update_job_status("3fa85f64-5717-4562-b3fc-2c963f66afa6")
-})
-
 </script>
 
     <template>
