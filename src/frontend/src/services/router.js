1ba3ea7f3950b98374f76d49d8d6c844d6098dce
Ce Zhang <zhangce.pku@gmail.com>
2022-09-07 12:00:00 -0500

    some UI functionality -- programmable prompts, job report page

--- src/frontend/src/services/router.js
+++ src/frontend/src/services/router.js
@@ -10,7 +10,7 @@ const routes = [
     { path: '/jobs', component: JobView},
     { path: '/status', component: DashboardView},
     { path: '/interactive', component: InteractiveView},
-    { path: '/report', component: ReportView},
+    { path: '/report/:jobid', component: ReportView},
 ]
 
 const router = createRouter({
