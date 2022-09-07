28f4c8fa6cc66d93999e7f5a7c32edbe0dff5871
zhangce <czhang@cs.stanford.edu>
2022-09-07 13:36:36 -0500

    some UI functionality -- programmable prompts, job report page (#2)
    Co-authored-by: Ce Zhang <zhangce.pku@gmail.com>

--- src/frontend/src/services/router.js
+++ src/frontend/src/services/router.js
@@ -3,12 +3,14 @@ import Landing from '../pages/Landing.vue';
 import JobView from '../pages/JobView.vue';
 import DashboardView from '../pages/DashboardView.vue';
 import InteractiveView from '../pages/Interactive.vue';
+import ReportView from '../pages/ReportView.vue';
 
 const routes = [
     { path: '/', component: Landing },
     { path: '/jobs', component: JobView},
     { path: '/status', component: DashboardView},
     { path: '/interactive', component: InteractiveView},
+    { path: '/report/:jobid', component: ReportView},
 ]
 
 const router = createRouter({
