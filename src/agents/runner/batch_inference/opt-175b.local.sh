878a11ff6977c3bff7b351a66ffdd1004d607aad
binhang <yuanbinhang@hotmail.com>
2022-09-20 12:55:01 +0200

    add scripts for local mode

---
+++ src/agents/runner/batch_inference/opt-175b.local.sh
@@ -0,0 +1,40 @@
+/root/fm/new/GPT-home-private    # Change directory
+
+netif='eth0'
+echo setting network interface to $netif
+
+job_id=$1
+
+echo job_id $job_id
+
+export NCCL_SOCKET_IFNAME=$netif
+export GLOO_SOCKET_IFNAME=$netif
+
+export NCCL_DEBUG=INFO
+export NCCL_IB_DISABLE=1
+export NCCL_P2P_DISABLE=1
+
+world_size=8
+machine_size=1
+n_gpu_per_machine=$((world_size/machine_size))
+
+i=0
+while :
+do
+  if [ "$i" -ge "$n_gpu_per_machine" ]; then
+      break
+  fi
+
+  DIST_CONF="--pp-mode pipe_sync_sample_mask_token_pipe --pipeline-group-size $world_size --cuda-id $i"
+  MODEL_CONF="--model-type opt --model-name /root/fm/new/models/opt-175b-new --num-iters 9999999999999"
+  INFERENCE_CONF="--fp16 --budget 20400 --batch-size 24 --input-seq-length 512 --generate-seq-length 32 --micro-batch-size 1 --num-layers 12 --max-layers 96"
+  COOR_CONF="--coordinator-server-ip 10.6.7.244 --working-directory /root/fm/new/working_dir --profiling no-profiling --net-interface $netif --job_id $job_id"
+
+  python -u dist_batch_and_latency_inference_w_httpclient.py $DIST_CONF $MODEL_CONF $INFERENCE_CONF $COOR_CONF &
+
+  ((port++))
+  ((i++))
+
+done
+
+wait
