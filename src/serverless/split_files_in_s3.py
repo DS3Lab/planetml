ca6f7769c4c59b8d4ea93fb5bfb9d1bea6c299e0
Jue WANG <zjuwangjue@gmail.com>
2022-09-20 16:30:45 +0800

    Update split_files_in_s3.py

--- src/serverless/split_files_in_s3.py
+++ src/serverless/split_files_in_s3.py
@@ -5,35 +5,6 @@ SPLIT_THRESHOLD = 1000
 SPLIT_CHUNKSIZE = 1000
 
 # This function capitalizes all text in the original object
-def lambda_handler_deprecated(event, context):
-    s3 = boto3.client('s3')
-    event = json.loads(event['body'])
-    with open(os.path.join("/tmp",event['file_id']), 'wb') as fp:
-        s3.download_fileobj(event['bucket'], event['file_id'], fp)
-    with open(os.path.join("/tmp",event['file_id']), 'r') as fp:
-        contents = [json.loads(x) for x in fp.readlines()]
-    
-    if len(contents) > SPLIT_THRESHOLD:
-        # here we start to split files into chunks, chunk size = 10
-        # we assume it has a either a 'prompt' or an 'input' keyword
-        # now sort them
-        if 'prompt' in contents[0]:
-            contents = sorted(contents, key=lambda d: len(d['prompt']))
-        elif 'input' in contents[0]:
-            contents = sorted(contents, key=lambda d: len(d['input']))
-        chunks = [contents[x:x+SPLIT_CHUNKSIZE] for x in range(0, len(contents), SPLIT_CHUNKSIZE)]
-        file_names = []
-        for idx, chunk in enumerate(chunks):
-            chunk_string = "\n".join([json.dumps(x) for x in chunk])
-            s3.put_object(Body=chunk_string, Bucket=event['bucket'], Key=event['file_id']+f"_{idx}")
-            file_names.append(event['file_id']+f"_{idx}")
-        return {'status_code': 200, 'original_length': len(contents), 'file_ids':file_names}
-    else:
-        return {'status_code': 100, 'original_length': len(contents), 'file_id':event['file_id']}
-    
-    return {'status_code': 200, 'original_length': len(contents)}
-
-
 def lambda_handler(event, context):
     s3 = boto3.client('s3')
     event = json.loads(event['body'])
@@ -45,8 +16,8 @@ def lambda_handler(event, context):
     clusters = defaultdict(list)
     for i, q in enumerate(contents):
         k = (q.get('model', None), q.get('echo', False), q.get('logprobs', 0), q.get('max_tokens', 1), q.get('n', 1), 
-             q.get('temperature', 0), q.get('best_of', 1), q.get('top_p', 1),
-             (tuple(sorted(q['stop'])) if q.get('stop', None) is not None else None)
+            q.get('temperature', 0), q.get('best_of', 1), q.get('top_p', 1),
+            (tuple(sorted(q['stop'])) if q.get('stop', None) is not None else None)
             )
         clusters[k].append(q)
 
@@ -63,12 +34,12 @@ def lambda_handler(event, context):
                 continue
             chunks.append(qs)
             
-     if len(chunks) > 1:
-         file_names = []
-         for idx, chunk in enumerate(chunks):
-             chunk_string = "\n".join([json.dumps(x) for x in chunk])
-             s3.put_object(Body=chunk_string, Bucket=event['bucket'], Key=event['file_id']+f"_{idx}")
-             file_names.append(event['file_id']+f"_{idx}")
-         return {'status_code': 200, 'original_length': len(contents), 'file_ids':file_names}
+    if len(chunks) > 1:
+        file_names = []
+        for idx, chunk in enumerate(chunks):
+            chunk_string = "\n".join([json.dumps(x) for x in chunk])
+            s3.put_object(Body=chunk_string, Bucket=event['bucket'], Key=event['file_id']+f"_{idx}")
+            file_names.append(event['file_id']+f"_{idx}")
+        return {'status_code': 200, 'original_length': len(contents), 'file_ids':file_names}
     else:
         return {'status_code': 100, 'original_length': len(contents), 'file_id':event['file_id']}
